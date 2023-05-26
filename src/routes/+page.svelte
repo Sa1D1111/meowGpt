23d414ed6c7dd6cceb03e27a8590e5bfee1420de
sa1D1111 <ofysaldelgado@hotmail.com>
2023-05-25 18:33:04 -0700

    prompts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -62,31 +62,39 @@
 	}
 </script>
 
-<div class="flex flex-col pt-4 w-full px-8 items-center gap-2">
-	<div>
-		<h1 class="text-2xl font-bold w-full text-center">Chatty</h1>
-		<p class="text-sm italic">Powered by gpt-3.5-turbo</p>
-	</div>
-	<div class="h-[500px] w-full bg-gray-900 rounded-md p-4 overflow-y-auto flex flex-col gap-4">
-		<div class="flex flex-col gap-2">
-			<ChatMessage type="assistant" message="Hello, ask me anything you want!" />
-			{#each chatMessages as message}
-				<ChatMessage type={message.role} message={message.content} />
-			{/each}
-			{#if answer}
-				<ChatMessage type="assistant" message={answer} />
-			{/if}
-			{#if loading}
-				<ChatMessage type="assistant" message="Loading.." />
-			{/if}
-		</div>
-		<div class="" bind:this={scrollToDiv} />
-	</div>
-	<form
-		class="flex w-full rounded-md gap-4 bg-gray-900 p-4"
-		on:submit|preventDefault={() => handleSubmit()}
-	>
-		<input type="text" class="input input-bordered w-full" bind:value={query} />
-		<button type="submit" class="btn btn-accent"> Send </button>
-	</form>
+<div>
+	<h1 class="text-2xl font-bold w-full text-center">chadGPT</h1>
 </div>
+<div class="chatwrapper  bg-white rounded-t-lg p-3 overflow-y-auto flex flex-col gap-4">
+	<div class="flex flex-col gap-2">
+		<ChatMessage type="assistant" message="meeeeaaaaow can i do for you?" />
+		{#each chatMessages as message}
+			<ChatMessage type={message.role} message={message.content} />
+		{/each}
+		{#if answer}
+			<ChatMessage type="assistant" message={answer} />
+		{/if}
+		{#if loading}
+			<ChatMessage type="assistant" message="Loading.." />
+		{/if}
+	</div>
+	<div class="" bind:this={scrollToDiv} />
+
+</div>
+<form
+	class="flex w-full rounded-b-lg gap-4 bg-white p-4 "
+	on:submit|preventDefault={() => handleSubmit()}
+>
+	<input type="text" class="input input-bordered w-full bg-slate-800" bind:value={query} />
+	<button type="submit" class="btn bg-blue-700 hover:bg-slate-800 text-white"> Send </button>
+</form>
+
+<style>
+.chatwrapper{
+height:40rem;
+width: 100%;
+
+
+}
+
+</style>
